15fe5b48591df80e2a007e95b1719279d4ff5314
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/homebrew_it.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Uninstall</source>
+        <translation>Disinstalla</translation>
+    </message>
+    <message>
+        <source>Install</source>
+        <translation>Installa</translation>
+    </message>
+    <message>
+        <source>Info (Browser)</source>
+        <translation>Info (Browser)</translation>
+    </message>
+    <message>
+        <source>Formula</source>
+        <translation>Formula</translation>
+    </message>
+    <message>
+        <source>Cask</source>
+        <translation>Cask</translation>
+    </message>
+    <message>
+        <source>Info (Terminal)</source>
+        <translation>Info (Terminale)</translation>
+    </message>
+    <message>
+        <source>Installed</source>
+        <translation>Installato</translation>
+    </message>
+    <message>
+        <source>Outdated</source>
+        <translation>Obsoleto</translation>
+    </message>
+    <message>
+        <source>DISABLED</source>
+        <translation>DISABILITATO</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Aggiorna</translation>
+    </message>
+    <message>
+        <source>Project homepage</source>
+        <translation>Homepage del progetto</translation>
+    </message>
+</context>
+</TS>
